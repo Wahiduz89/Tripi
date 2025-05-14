8b952becdb1263fc5bcc3ef02eb58b3235e22033
Wahiduz Zaman <156948092+Wahiduz89@users.noreply.github.com>
2025-05-14 21:48:15 +0530

    Update News.tsx

--- components/Home/News/News.tsx
+++ components/Home/News/News.tsx
@@ -33,7 +33,7 @@ const News = () => {
         >
           <NewsCard
             image="/images/n3.jpg"
-            title="Top 10 place to visit in Russia"
+            title="Top 10 place to visit in Dubai"
             date="22 November 2024"
           />
         </div>
